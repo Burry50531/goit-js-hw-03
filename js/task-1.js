d4dd206671055044ee67ae389935810ce852a5fa
Burry50531 <165029477+Burry50531@users.noreply.github.com>
2024-09-17 13:32:41 +0300

    js3

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+function slugify(title) {
+    return title
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, '-')
+        .replace(/[^\w\-]+/g, '')
+        .replace(/\-\-+/g, '-')
+        .replace(/^-+/, '')
+        .replace(/-+$/, '');
+}
+
+console.log(slugify("Arrays for beginners"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
